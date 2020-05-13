693cb4f05d7aee21f5e92d8392db5f6f6de74f2c
punkator <oscarfabiancadena@gmail.com>
2020-05-13 07:56:12 -0500

    se modifica componente PHP

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -2,4 +2,5 @@
 print "Hello, World! php versiosdn is " . PHP_VERSION . "\n";
 print "A change is in the air!\n";
 print "SE AGREGA IMPLEMETACION A ARTEFACTO PHP  DESDE ORIGIN OFCC_CONSOLE!\n";
+print "SE AGREGA NUEVA  IMPLEMETACION A ARTEFACTO PHP  DESDE ORIGIN OFCC_CONSOLE...!\n";
 ?>
